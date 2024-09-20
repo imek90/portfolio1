5672969dc7f4bfe1d808bcbd1a5f8cff9e107e92
imek90 <58920843+imek90@users.noreply.github.com>
2024-09-20 19:46:53 -0400

    2.4 - 4.2 projects

---
+++ HTTP Requests/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>")
+})
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>info about me stuff...</p>")
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1>")
+});
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
